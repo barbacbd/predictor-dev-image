e78ff9d224f9bb9b8e4e815fc177e331f02a2382
Brent Barbachem <barbacbd@dukes.jmu.edu>
2022-08-29 21:02:09 -0400

    First commit, moving it over after testing from predictor.

---
+++ CreateDockerfile.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Build a Docker/Podman image that contains the data for development
+# in the predictor project
+set -eux
+
+# Print with extra color
+function LOG() {
+    echo -e "\033[0;34m${1}\033[0m"
+}
+
+# Variables used throughout the course of the program
+EMAIL="barbacbd@dukes.jmu.edu"
+FULLNAME="Brent Barbachem"
+IMAGE="predictor-dev"
+DockerFileCreated="False"
+ImageBuilt="False"
+DockerFileName="DockerfileDev"
+
+
+# Determine if an image already exists ...
+#FoundImages=$(podman image ls | grep "${IMAGE}")
+#LOG "${FoundImages}"
+#IMAGE="golang"
+FoundImages=$(podman image ls | grep "${IMAGE}" | wc -l)
+LOG "${FoundImages}"
+
+if [ $FoundImages -gt 0 ]; then
+    LOG "delete or update image before proceeding: ${IMAGE}"
+else
+    # Build the dockerfle specifically for development
+    if [ ! -f "${DockerFileName}" ]; then
+	LOG "creating Dockerfile ..."
+	cat <<EOF >${DockerFileName}
+from fedora:latest
+MAINTAINER "${FULLNAME}"
+
+# Update all packages
+RUN dnf update -y
+
+# R is a dependency to this package and must be installed
+# prior to installing the R-python pacakge.
+RUN dnf install -y \
+    R \
+    python3-devel \
+    python3-pip \
+    git \
+    openssh \
+    openssh-clients \
+    emacs \
+    vim \
+    gcc \
+    gcc-c++
+
+# Grab the lastest package source.
+RUN git clone https://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@github.com/barbacbd/predictor.git
+
+# Grab my specific source code for the FEAST project
+# I forked this project (do NOT own it) and have made my modifications
+# so that there is an extensive python extension.
+# Pull that data here and build the source on this vm
+RUN git clone https://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@github.com/barbacbd/FEAST.git
+RUN cd FEAST && git checkout py_update_setup_for_future_pys
+RUN cd FEAST/python && bash -c "./build.sh"
+
+# upgrade pip
+RUN python3 -m pip install pip --upgrade
+
+# install the project requirements
+RUN python3 -m pip install -r predictor/requirements.txt
+
+RUN git config --global user.email "${EMAIL}"
+RUN git config --global user.name "${FULLNAME}"
+
+EOF
+
+	# Set that the dockerfile was created so we can clean it up later
+	DockerFileCreated="True"
+    fi
+
+    LOG "building image ..."
+    podman build -f ./${DockerFileName} -t ${IMAGE}:latest
+    ImageBuilt="True"
+
+    # Cleanup any Dockerfile that may have been created
+    if [[ "${DockerFileCreated}" == "True" ]]; then
+	LOG "destroying dockerfile ..."
+	rm DockerfileDev
+    else
+	LOG "no dockerfile created, skipping destruction ..."
+    fi
+
+fi
+
+
+# Provide final feedback to the user
+if [[ "${ImageBuilt}" == "True" ]]; then
+    LOG "podman run -it ${IMAGE}:latest /bin/bash"
+else
+    LOG "no image built ..."
+fi
---
+++ README.md
@@ -0,0 +1,70 @@
+<h1 align="center">
+<br>Containerization</br>
+  <a href="https://www.docker.com/">
+    <img src=".images/docker.png" width="100" border-radius="50%"/>
+  </a>
+  <a href="https://podman.io/">
+    <img src=".images/podman.png" width="100" border-radius="50%"/> 
+  </a>
+</h1>
+
+The project contains the instructions for creating the [docker](https://www.docker.com/) or [podman](https://podman.io/) image and running the container instance of the image.
+
+**Note**: _Podman and Docker can be used interchangeably throughout this document, unless specified otherwise_.
+
+# Initialization
+
+
+```bash
+./CreateDockerfile.sh
+```
+
+The script listed [above](./CreateDockerfile.sh) will build a Dockerfile and create the image. The Dockerfile is removed
+at the end of the script, if it was created. At the conclusion of the script, the user will be prompted with a similar
+line to the one below.
+
+
+```bash
+podman run -it predictor-dev:latest /bin/bash
+```
+
+This line/command can and should be used to enter the container through bash.
+
+
+_If you wish to call your image something other than `predictor-dev`, change the name `predictor-dev` in the script_.
+
+
+# Checking the image
+
+You may check that your image was created with:
+
+```bash
+podman image ls
+```
+
+You _should_ receive an output such as:
+
+```bash
+REPOSITORY                          TAG          IMAGE ID      CREATED        SIZE
+localhost/predictor-dev             latest       8eaeb0d43eed  2 days ago     3.18 GB
+```
+
+If your output looks similar to below, then your image was **not** created successfully. If/When this occurs, please look carefully at
+the log during image creation and report any issues.
+
+```bash
+REPOSITORY                          TAG          IMAGE ID      CREATED        SIZE
+<none>                              <none>       9989387fbb3e  3 weeks ago    395 MB
+```
+
+# Passing data to the container
+
+```bash
+podman run -it --privileged -v ${PWD}/predictor_output:/predictor_output predictor:latest /bin/bash
+```
+
+This will execute the container and enter you into the container. _Any file(s) that you wish to share between the host and container should be
+added to the directory that you created_.
+
+
+**Note**: _You only need to add `--privileged` when using [podman](https://podman.io/) as the permissions are handled differently between podman and docker_.
